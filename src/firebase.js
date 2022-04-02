b7ba9c904b63e46dd126bc61bd0c4f37896845d0
Shoaib Mirza <shoaibmirjagtp@gmail.com>
2022-04-02 11:43:03 +0530

    fix up

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCC1sLrxqPLIJGQP7M5jd2X3B6mSjJvN6I",
-  authDomain: "disneyplus-clone-d74fc.firebaseapp.com",
-  projectId: "disneyplus-clone-d74fc",
-  storageBucket: "disneyplus-clone-d74fc.appspot.com",
-  messagingSenderId: "238842038719",
-  appId: "1:238842038719:web:0d34ddb0ff5eb30cf2b90a",
-  measurementId: "G-LEQKRWL4M2",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
